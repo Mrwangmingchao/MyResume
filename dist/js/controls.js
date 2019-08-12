be9a7c640ae27159de430262dcf95c8842cd60a6
Mrwang <1254225149@qq.com>
2019-08-12 12:53:10 +0800

    uglify

---
+++ dist/js/controls.js
@@ -0,0 +1,1 @@
+$(function(){$(".simplefilter li").click(function(){$(".simplefilter li").removeClass("active"),$(this).addClass("active")}),$(".multifilter li").click(function(){$(this).toggleClass("active")}),$(".shuffle-btn").click(function(){$(".sort-btn").removeClass("active")}),$(".sort-btn").click(function(){$(".sort-btn").removeClass("active"),$(this).addClass("active")})});
